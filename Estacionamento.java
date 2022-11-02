22b6ba9e6a10f4ee1459619ce13b4d72978837f0
Gabriel <bielsobv@gmail.com>
2022-11-02 14:14:31 -0300

    adição de comandos no prog estacionamento

--- Estacionamento.java
+++ Estacionamento.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.crypto.ExemptionMechanismException;
+
 public class Estacionamento {
 
     public static void main(String[] args){
@@ -96,8 +98,6 @@ public class Estacionamento {
 
     public static void removeCarro (Scanner scanner){
         System.out.println("Remover Carro");
-        System.out.println("Informe a placa: ");
-        String placa = scanner.next();
         System.out.println("Digite o ID do carro: ");
         int idCarro = scanner.nextInt();
         try{
@@ -121,6 +121,145 @@ public class Estacionamento {
         new Moto(idMoto, nome, placa, cilindrada);
         }catch (Exception e){
             System.out.println("Erro ao cadastrar Moto");
+        }  
+    }
+    public static void listarMoto(){
+        for(Moto moto : Moto.motos){
+            System.out.println(moto);
+        }
+    }
+
+
+    public static void removeMoto (Scanner scanner){
+        System.out.println("Remover Moto");
+        System.out.println("Digite o ID da moto: ");
+        int idMoto = scanner.nextInt();
+        try{
+            Moto.removeMoto(idMoto);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void cadastrarBicicleta(Scanner scanner){
+        try{
+            System.out.println("Cadastrar Bicicleta");
+            System.out.println("Digite o ID da Bicicleta: ");
+            int id = scanner.nextInt();
+            System.out.println("Nome: ");
+            String nome = scanner.next();
+            System.out.println("Marca: ");
+            String marca = scanner.next();
+            System.out.println("Cor: ");
+            String cor = scanner.next();
+
+            new Bicicleta(id, nome, marca, cor);
+        }catch(Exception e){
+            System.out.println("Erro ao cadastrar a Bicicleta");
+        }
+    }
+
+    public static void listarBicicleta(){
+        for (Bicicleta bicicleta : Bicicleta.bicicletas){
+            System.out.println(bicicleta);
+        }    
+    }
+
+    public static void removeBicicleta(Scanner scanner){
+        System.out.println("Remover Bicicleta");
+        System.out.println("Informe o ID da Moto: ");
+        int idBicicleta = scanner.nextInt();
+        try{
+            Bicicleta.removeBicicleta(idBicicleta);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void cadastrarVaga(Scanner scanner){
+        try{
+            System.out.println("Cadastrar Vaga");
+            System.out.println("Digite o ID da vaga: ");
+            int id = scanner.nextInt();
+            System.out.println("Número da vaga: ");
+            String numero = scanner.next();
+            System.out.println("Tamanho: ");
+           
+        System.out.println("Digite a quantidade de vagas: ");
+        int quantidade = scanner.nextInt();
+
+        for(int i = 0; i < quantidade; i++){
+            System.out.println("Tipo de veículo: ");
+            String tipo = scanner.next();
+            switch(tipo){
+                case "C":
+                    tipo = "Carro";
+                    break;
+                case "M":
+                    tipo = "Moto";
+                    break;
+                case "B":
+                    tipo = "Bicicleta";
+                    break;
+                default:
+                    throw new Exception("Tipo inválido!");    
+            }
+            String tamanho = scanner.next();
+            System.out.println("Preço: ");
+            String preço = scanner.next();
+
+            new Vaga(numero, tipo, tamanho, preço);
+        }
+        }catch(Exception e){
+            System.out.println("Erro ao cadastrar a Vaga");
+        }
+    }
+
+    public static void listarVaga(){
+        for(Vaga vaga : Vaga.vagas){
+            System.out.println(vaga);
+        }
+    }
+
+    public static void removeVaga (Scanner scanner){
+        System.err.println("Remover Vaga");
+        System.out.println("Digite o ID da Vaga: ");
+        int idVaga = scanner.nextInt();
+        try{
+            Vaga.removeVaga(idVaga);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void cadastrarLocação (Scanner scanner){
+        try{
+        System.out.println("Cadastrar Locação");
+        System.out.println("Digite o ID da locação");
+        int id = scanner.nextInt();
+        System.out.println("Data da locação: ");
+        String data = scanner.next();
+
+        new Locacao(id, data);
+        }catch (Exception e){
+            System.out.println("Erro ao cadastrar a Locação!");
+        }
+    }
+
+    public static void listarLocação(){
+        for(Locacao locacao : Locacao.locacoes){
+            System.out.println(locacao);
+        }
+    }
+
+    public static void removeLocacao(Scanner scanner){
+        System.out.println("Remover Locação");
+        System.out.println("Digite o ID da locação: ");
+        int id = scanner.nextInt();
+        try{
+            Locacao.removeLocacao(id);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
